edff5ccd166b97c23df83216ff5a95bad30fc43e
Alexander Noriega <lambdatoast@gmail.com>
2014-04-28 23:45:52 -0300

    Add functions for converting query parameters to their URL versions. Use the Query abstraction as input to the query function.

--- src/main/scala/SolrStream.scala
+++ src/main/scala/SolrStream.scala
@@ -11,19 +11,37 @@ object SolrStream {
   sealed trait CommonQueryParameter
   final case class Q(value: String) extends CommonQueryParameter
 
+  object CommonQueryParam {
+    def toURLQuery(qp: CommonQueryParameter): String =
+      qp match {
+        case Q(v) => "q" + "=" + v
+      }
+  }
+
   /** [[http://wiki.apache.org/solr/CoreQueryParameters CommonQueryParameters]]
     */
   sealed trait CoreQueryParameter
   final case class Wt(value: String) extends CoreQueryParameter
+
+  object CoreQueryParam {
+    def toURLQuery(qp: CoreQueryParameter): String =
+      qp match {
+        case Wt(v) => "wt" + "=" + v
+      }
+  }
   
   /** Solr query definition.
     * [[http://wiki.apache.org/solr/QueryParametersIndex Query parameters docs]]
     */
   case class Query(commonParams: Set[CommonQueryParameter], coreParams: Set[CoreQueryParameter])
-  
-  def query(url: String): Process[Task, String] =
-    io.linesR(Source.fromURL(url, "utf-8"))
 
-  def example = query("http://localhost:8983/solr/collection1/select?q=*%3A*&wt=json&indent=true")
+  def queryToString(q: Query): String = 
+    q.commonParams.map(CommonQueryParam.toURLQuery(_)).mkString("&") + 
+    "&" + q.coreParams.map(CoreQueryParam.toURLQuery(_)).mkString("&")
+  
+  def query(url: String)(q: Query): Process[Task, String] =
+    io.linesR(Source.fromURL(url + "?" + queryToString(q), "utf-8"))
+
+  def example = query("http://localhost:8983/solr/collection1/select")(Query(Set(Q("*:*")), Set(Wt("json"))))
 
 }
