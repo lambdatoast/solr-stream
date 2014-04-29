e534a77072a871552a0e43b704f7577c88c719c3
Alexander Noriega <lambdatoast@gmail.com>
2014-04-28 23:17:27 -0300

    Start adding basic abstractions for query parameters.

--- src/main/scala/SolrStream.scala
+++ src/main/scala/SolrStream.scala
@@ -5,6 +5,21 @@ import scalaz.concurrent.Task
 import scala.io.Source
 
 object SolrStream {
+
+  /** [[http://wiki.apache.org/solr/CommonQueryParameters CommonQueryParameters]]
+    */
+  sealed trait CommonQueryParameter
+  final case class Q(value: String) extends CommonQueryParameter
+
+  /** [[http://wiki.apache.org/solr/CoreQueryParameters CommonQueryParameters]]
+    */
+  sealed trait CoreQueryParameter
+  final case class Wt(value: String) extends CoreQueryParameter
+  
+  /** Solr query definition.
+    * [[http://wiki.apache.org/solr/QueryParametersIndex Query parameters docs]]
+    */
+  case class Query(commonParams: Set[CommonQueryParameter], coreParams: Set[CoreQueryParameter])
   
   def query(url: String): Process[Task, String] =
     io.linesR(Source.fromURL(url, "utf-8"))
