9128abb0d6089e6bdf2676cdc20b39dd6ade1598
Alexander Noriega <lambdatoast@gmail.com>
2014-05-09 17:37:30 -0300

    Add FacetSort query parameter models.

--- src/main/scala/solrstream/QueryParameter.scala
+++ src/main/scala/solrstream/QueryParameter.scala
@@ -4,6 +4,7 @@ import java.net.URLEncoder
 /** 
   * [[http://wiki.apache.org/solr/CommonQueryParameters CommonQueryParameters]]
   * [[http://wiki.apache.org/solr/CoreQueryParameters CoreQueryParameters]]
+  * [[http://wiki.apache.org/solr/SimpleFacetParameters SimpleFacetParameters]]
   */
 sealed trait QueryParameter
 final case class Q(value: String) extends QueryParameter
@@ -11,6 +12,10 @@ final case class Wt(value: String) extends QueryParameter
 final case class Fl(value: Set[String]) extends QueryParameter
 final case object Facet extends QueryParameter
 final case class FacetField(value: String) extends QueryParameter
+sealed trait FacetSortOption
+final case object Count extends FacetSortOption
+final case object Index extends FacetSortOption
+final case class FacetSort(value: FacetSortOption) extends QueryParameter
 
 object QueryParameter {
   def kv(k: String)(v: String): String = k + "=" + URLEncoder.encode(v, "utf-8")
@@ -21,6 +26,10 @@ object QueryParameter {
       case Fl(vs) => kv("fl")(vs.mkString(","))
       case Facet => kv("facet")("true")
       case FacetField(v) => kv("facet.field")(v)
+      case FacetSort(v) => kv("facet.sort")(v match {
+        case Index => "index"
+        case Count => "count"
+      })
     }
 }
 
